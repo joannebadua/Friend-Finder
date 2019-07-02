1afac6c4be560100253ff85ec58efcf896c5ac1a
Joanne Badua <joannebadua@gmail.com>
2019-07-01 23:27:06 -0700

    add form-control style to user answer and create var answers, answersarray and newfriend in survey.html

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,15 +4,18 @@
 // Initially we just set it equal to a "dummy" user.
 // ===============================================================================
 
-var tableArray = [
+var friendsArray = [
     {
       userName: "esquire",
-      userEmail: "esquire@example.com",
-      userID: "law1234",
-      phoneNumber: "000-000-0000"
+      phoneNumber: "esquire@example.com",
+      userEmail: "law1234",
+      practiceArea: "000-000-0000",
+      cityState: "Honolulu, HI",
+      // answers: [1,3,4,2,2,5,2,3,4,1]
+
     }
   ];
   
   // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = tableArray;
+  module.exports = friendsArray;
   
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports = function(app){
         res.sendFile(path.join(__dirname, "/../public/home.html"))
     })
     
-    app.get("/add", function(req, res) {
+    app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname, "/../public/survey.html"))
     })
 }
