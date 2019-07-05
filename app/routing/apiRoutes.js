348e39f8aa7482096a90d8b7cb1cece42a43a2bf
Joanne Badua <joannebadua@gmail.com>
2019-07-04 18:22:27 -0700

    keep route names consistent

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,17 +5,17 @@
 
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-var friendsData = require("../data/friends");
+var friendsData = require("../data/friends.js");
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 module.exports = function (app) {
-    app.get("/api/friends", function (req, res) {
+    app.get("/data/friends", function (req, res) {
         res.json(friendsData);
     });
 
-    app.post("/api/friends", function (req, res) {
+    app.post("/data/friends", function (req, res) {
         console.log(req.body)
         var newFriendTotal = 0;
         for (let index = 0; index < req.body.answers.length; index++) {
@@ -41,6 +41,7 @@ module.exports = function (app) {
         };
         console.log("your best friend", bestFriend);
 
+        //pushed in the array
         friendsData.push(req.body);
         res.json(bestFriend);
     });
